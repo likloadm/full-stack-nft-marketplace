9dac64c41cf65605f5e993d49adf3809e125c53c
Vasilii <liklo.adm@gmail.com>
2022-10-16 21:58:53 +0300

    update qtum deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,28 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+ const privateKey = fs.readFileSync(".secret").toString().trim() || "";
+ const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: "qtum",
   networks: {
     hardhat: {
       chainId: 1337
     },
-    /*
-    mumbai: {
+
+    qtum: {
       // Infura
       // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
+      url: "https://eth2.quark.blue:23890",
       accounts: [privateKey]
     },
-    matic: {
-      // Infura
-      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
-      accounts: [privateKey]
-    }
-    */
+//    matic: {
+//      // Infura
+//      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+//      url: "https://rpc-mainnet.maticvigil.com",
+//      accounts: [privateKey]
+//    }
+//    */
   },
   solidity: {
     version: "0.8.4",
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,14 +3,20 @@ const fs = require('fs');
 
 async function main() {
   const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+  console.log("factory")
   const nftMarket = await NFTMarket.deploy();
+  console.log("deploy", nftMarket.address)
   await nftMarket.deployed();
   console.log("nftMarket deployed to:", nftMarket.address);
+  console.log("getContractAddress:", nftMarket.contractAddress);
+  // console.log("nftMarket:", nftMarket);
 
   const NFT = await hre.ethers.getContractFactory("NFT");
   const nft = await NFT.deploy(nftMarket.address);
+  console.log("deploy", nft.address)
   await nft.deployed();
   console.log("nft deployed to:", nft.address);
+  console.log("getContractAddress:", nft.contractAddress);
 
   let config = `
   export const nftmarketaddress = "${nftMarket.address}"
